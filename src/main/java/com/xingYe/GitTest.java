2b11f8f6fcc1d084a174d5fa305e494b889dba00
xinRan <2831334704@qq.com>
2023-01-17 13:58:54 +0800

    first commmit

---
+++ src/main/java/com/xingYe/GitTest.java
@@ -0,0 +1,17 @@
+package com.xingYe;
+
+/**
+ * @Date: 2023/1/17 13:53
+ * @Description:
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+        System.out.println(sum(7.2,3.4));
+    }
+
+    public static double sum(double a,double b){
+        return a+b;
+    }
+}
