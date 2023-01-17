f43bef0c814ebee915279cc47d55a833b4664a9c
xinRan <2831334704@qq.com>
2023-01-17 15:54:24 +0800

    master third commit

--- src/main/java/com/xingYe/GitTest.java
+++ src/main/java/com/xingYe/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println(sum(7.2,3.4));
         System.out.println("this is hot-fix fork");
         System.out.println("this is hot-fix fork print message");
+        System.out.println("new creat version of master fork");
     }
 
     public static double sum(double a,double b){
